ace61d6674523b6d1033a35e283ae5ba77d7c614
Min Park <minsungpark.msp@gmail.com>
2021-08-25 23:16:48 -0600

    Complete order.js and order test file

--- src/order.js
+++ src/order.js
@@ -1,7 +1,40 @@
+function takeOrder(order, orders) {
+  if (orders.length < 3) {
+    orders.push(order)
+
+  }
+}
+
+function refundOrder(number, deliveryOrders) {
+  for (var i = 0; i < deliveryOrders.length; i++) {
+    if (deliveryOrders[i].orderNumber === number) {
+      deliveryOrders.splice(i, 1);
+    }
+  }
+  return deliveryOrders;
+}
+
+function listItems(orders) {
+  var items = [];
+  for (var i = 0; i < orders.length; i++) {
+    items.push(orders[i].item);
+  }
+  return items.join(", ");
+}
+
+function searchOrder(list, order) {
+  var isListed = false;
+  for (var i = 0; i < list.length; i++) {
+    if (list[i].item === order) {
+      isListed = true;
+    }
+  }
+  return isListed;
+}
 
 module.exports = {
-  // takeOrder,
-  // refundOrder,
-  // listItems,
-  // searchOrder
+  takeOrder,
+  refundOrder,
+  listItems,
+  searchOrder
 }
